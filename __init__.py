6738b13b337cf8b9a1bb3ab7d190c7053fde7986
Matthew McLaughlin <matthedm@uci.edu>
2025-02-24 18:31:00 -0800

    insturciots and fixed accidental removal of connect search hanzi viewer

--- __init__.py
+++ __init__.py
@@ -14,37 +14,6 @@ from .config import has, get
 
 CON = DatabaseController()
 
-
-
-def add_hanzi_info_to_template(card_layout):
-    """
-        Checks both the question and answer sides of the card for a div tag with id=hanzihome.
-        If found, it will add JavaScript and HTML required to show info about each character in the 'hanzi' field.
-        :param card_layout:
-        :return:
-        """
-    # Check both question and answer templates
-    for side in ["qfmt", "afmt"]:
-        html = card_layout.current_template()[side]
-        soup = BeautifulSoup(html, "html.parser")
-        home_div = soup.find("div", {"id": "hanzihome"})
-        if home_div:
-            # Found the div, proceed with customization
-            copy_resources_to_media_folder()
-            # Use hanziSource field... look up each character in the JSON
-            return
-    # If no div is found in either template
-    showInfo("Could not find div with id='hanzihome' in question or answer template.")
-    #using hanziSource field...look up each character in the json
-
-
-def add_custom_button_to_card_layout(card_layout):
-    # Create the button
-    btn = QPushButton("Add Hanzi Info")
-    btn.clicked.connect(lambda: add_hanzi_info_to_template(card_layout))
-    # Add the button to the bottom buttons area
-    card_layout.buttons.addWidget(btn)  # `buttons` is already a QHBoxLayout
-
 def on_lookup_hanzi(selected_text, db: DatabaseController):
     # Replace this with your Hanzi lookup function or URL
     mw.hanzi_viewer = v = HanziViewer(selected_text, db)
@@ -107,6 +76,10 @@ def main():
         print("Config file created.")
     else:
         print("Config file already exists.")
+
+    # Hanzi Viewer
+    gui_hooks.webview_will_show_context_menu.append(on_webview_will_show_context_menu)
+    # Searching in browser
     gui_hooks.webview_will_show_context_menu.append(on_webview_context_menu)
     gui_hooks.editor_will_show_context_menu.append(on_webview_context_menu)
     
